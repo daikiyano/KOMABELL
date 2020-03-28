abbf503e2986a34a142529bec12eafe6b6a8d3ed
矢野大樹 <daikitech0123@gmail.com>
2020-03-28 16:28:51 +0900

    Add Edit Page

--- app/vue-project/src/components/professors/Edit.vue
+++ app/vue-project/src/components/professors/Edit.vue
@@ -4,38 +4,17 @@
     <el-tab-pane label="アカウント情報" name="Profile"></el-tab-pane>
     <el-tab-pane label="スケジュール調整" name="Schedule"></el-tab-pane>
   </el-tabs>
-  <component :is="currentView"></component>
-    <el-card style="width:80%; margin: 0 auto; background-color: rgba(0, 0, 0, 0);" class="box-card">
-      <el-form :label-position="labelPosition" label-width="100px">
-        <!-- <el-input :disabled="true" v-model="professor.major"></el-input> -->
-        <el-form-item label="学部学科">
-        <span v-if="professor.major===1">仏教学部禅学科</span>
-        <span v-else-if="professor.major===2">文学部国文学科</span>
-         <span v-else-if="professor.major===3">文学部地理学科</span>
-          <span v-else-if="professor.major===4">文学部社会学科</span>
-          <span v-else-if="professor.major===5">文学部英米文学科</span>
-                <span v-else-if="professor.major===6">文学部歴史学科</span>
-                <span v-else-if="professor.major===7">文学部心理学科</span>
-                <span v-else-if="professor.major===8">経済学部経済学科</span>
-                <span v-else-if="professor.major===9">経済学部商学科</span>
-                <span v-else-if="professor.major===10">経済学部現代応用経済学科</span>
-                <span v-else-if="professor.major===11">法学部法律学科</span>
-                <span v-else-if="professor.major===12">法学部政治学科</span>
-                <span v-else-if="professor.major===13">経営学部経営学科</span>
-                <span v-else-if="professor.major===14">経営学部市場戦略学科</span>
-                <span v-else-if="professor.major===15">医療健康科学部診療放射線技術科学学科</span>
-                <span v-else-if="professor.major===16">グローバルメディアスタディーズ学部グローバルメディアスタディーズ学科</span>
-                 </el-form-item>
-        <el-form-item label="教授名">
-          <el-input placeholder="Please input" v-model="professor.name"></el-input>
-        </el-form-item>
-        <el-form-item label="メールアドレス">
-          <el-input placeholder="Please input" v-model="user.email"></el-input>
-        </el-form-item>
-        <el-button type="success">Success</el-button>
-      </el-form>
-    </el-card>                
-
+  <component 
+      :is="currentView" 
+      :professor="this.professor" 
+      :labelPosition="this.labelPosition" 
+      :user="this.user"
+      :Isdisabled="this.Isdisabled"
+      @ChangeEditInput="ChangeEditInput"
+      @ChangeDisable="ChangeDisable"
+      >
+      
+  </component>            
   </div>
 </template>
 <script>
@@ -53,17 +32,50 @@ export default {
   },
   data () {
     return {
-        professor : [],
-        user : [],
-        labelPosition: 'top',
-         currentView: 'Profile' 
+      currentView: 'Profile',
+      professor: {},
+      user : {},
+      labelPosition: 'top',
+      Isdisabled : true
     }
   },
   methods: {
     handleClick(tab) {
         console.log(tab.name);
          this.currentView = tab.name
-      }
+    },
+    ChangeDisable(Isdisabled) {
+      this.Isdisabled = Isdisabled
+    },
+   
+    ChangeEditInput (UpdateInput) {
+      this.professor = UpdateInput
+      let currentUser = firebase.auth().currentUser
+      let self = this
+      db.collection("professors").where("user_id", "==", currentUser.uid)
+      .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            db.collection('professors').doc(doc.id).update({
+              name: self.professor.name, 
+              comment: self.professor.comment
+            })
+            .then(() => {
+              self.$message({
+              message: 'プロフィールを更新しました！',
+              type: 'success'
+            });
+              self.Isdisabled = true
+            }).catch(err => {
+              console.log(err)
+            })
+          });
+        })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+      });
+    }
   },
   created(){
     let self = this
--- app/vue-project/src/components/professors/Profile.vue
+++ app/vue-project/src/components/professors/Profile.vue
@@ -1,3 +1,122 @@
 <template>
-  <div><h1>Home Page</h1></div>
+<div>
+    <el-card style="width:60%; margin: 0 auto; background-color: rgba(0, 0, 0, 0);" class="box-card">
+      <el-form :label-position="labelPosition" label-width="100px">
+        <!-- <el-input :disabled="true" v-model="professor.major"></el-input> -->
+        <el-form-item label="学部学科">
+        <span v-if="professor.major===1">仏教学部禅学科</span>
+        <span v-else-if="professor.major===2">文学部国文学科</span>
+        <span v-else-if="professor.major===3">文学部地理学科</span>
+        <span v-else-if="professor.major===4">文学部社会学科</span>
+        <span v-else-if="professor.major===5">文学部英米文学科</span>
+        <span v-else-if="professor.major===6">文学部歴史学科</span>
+        <span v-else-if="professor.major===7">文学部心理学科</span>
+        <span v-else-if="professor.major===8">経済学部経済学科</span>
+        <span v-else-if="professor.major===9">経済学部商学科</span>
+        <span v-else-if="professor.major===10">経済学部現代応用経済学科</span>
+        <span v-else-if="professor.major===11">法学部法律学科</span>
+        <span v-else-if="professor.major===12">法学部政治学科</span>
+        <span v-else-if="professor.major===13">経営学部経営学科</span>
+        <span v-else-if="professor.major===14">経営学部市場戦略学科</span>
+        <span v-else-if="professor.major===15">医療健康科学部診療放射線技術科学学科</span>
+        <span v-else-if="professor.major===16">グローバルメディアスタディーズ学部グローバルメディアスタディーズ学科</span>
+        </el-form-item>
+        <el-form-item label="教授名">
+          <el-input placeholder="Please input"  
+            @input="updateNameInput" 
+            v-model="professor.name">
+          </el-input>
+        </el-form-item>
+          <p v-if="feedback">{{ feedback }}</p>
+        <!-- <el-form-item label="メールアドレス">
+          <el-input placeholder="Please input" v-model="user.email"></el-input>
+        </el-form-item> -->
+        <el-form-item label="コメント">
+          <el-input
+            type="textarea"
+            :rows="10"
+            placeholder="Please input"
+            @input="updateCommentInput" 
+            v-model="professor.comment">
+          </el-input>
+        </el-form-item>
+        <el-button @click="ChangeEditInput" type="success" :disabled="this.Isdisabled == true">更新</el-button>
+      </el-form>
+    </el-card>                
+
+</div>
 </template>
+<script>
+/* eslint-disable no-console */
+
+// import db from '../../../firebase/init.js'
+// import firebase from 'firebase'
+
+export default {
+  name: 'Profile',
+  props: {
+    'professor': {
+       type: Object,
+       required: true
+    },
+    'labelPosition': {
+       type: String,
+       required: true
+
+    },
+    'user': {
+       type: Object,
+       required: true
+
+    },
+    'Isdisabled': {
+       type: Boolean,
+       required: true
+    }
+  },
+  data () {
+    return {
+      feedback: null,
+      oldName : this.professor.name,
+      oldComment : this.professor.name,
+      disabled : null
+    }
+  },
+  methods:{
+    ChangeEditInput () {
+      if(this.professor.name){
+        this.$emit('ChangeEditInput',this.professor)
+        this.feedback = null
+      } else {
+        this.feedback = "名前を入力してください"
+      }
+    },
+    updateNameInput (input) {
+      if (input!==this.oldName)
+        this.$emit('ChangeDisable',false)
+    },
+    updateCommentInput (input) {
+      if (input!==this.oldComment)
+        this.$emit('ChangeDisable',false)
+    }
+   
+  },
+  mounted () {
+     
+     
+  },
+  created(){
+   
+  } 
+}
+
+
+</script>
+ 
+
+ <style lang="scss">
+
+
+</style>
+
+
